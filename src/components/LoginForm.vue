7569be90a425b957df6ff910271d610aeede1b34
Gabriel Soares <gabrielrs.job@gmail.com>
2023-08-06 21:41:14 -0300

    style: cores customizadas

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex justify-center items-center mt-4">
+    <div class="bg-[white] flex justify-center items-center mt-4">
         <div class="h-[500px] md:h-[600px] center w-[320px] bg-white rounded-xl shadow-md">
             <div class="flex flex-col items-center gap-3 mt-6 ">
                 <img class="h-7 w-7" src="../assets/imgs/circle-orkut.png" alt="O do Orkut">
-                <h1 class="text-[#ED2590] font-semibold text-xl">Acesse o Orkut</h1>
+                <h1 class="text-orkut-pink font-semibold text-xl">Acesse o Orkut</h1>
             </div>
             <form>
                 <div class="flex flex-col justify-center items-center gap-4 mt-12">
@@ -13,9 +13,9 @@
                         <input type="checkbox" name="lembrarSenha" >
                         <label for="lembrarSenha" class="text-[#868686]">Lembrar minha senha</label>
                     </div> 
-                    <button @click="router.push('/')" class="bg-[#ED2590] w-[280px] h-[38px] rounded-md text-white" type="submit">Entrar na conta</button>
-                    <button class="bg-[#EFF3F8] w-[280px] h-[38px] rounded-md text-[#ED2590] font-medium" type="submit">Criar uma conta</button>
-                    <a class="text-[#ED2590] underline mt-10" href="#">Esqueci minha senha</a>
+                    <button @click="router.push('/')" class="text-white bg-orkut-pink w-[280px] h-[38px] rounded-md " type="submit">Entrar na conta</button>
+                    <button class="bg-[#EFF3F8] w-[280px] h-[38px] rounded-md text-orkut-pink font-medium" type="submit">Criar uma conta</button>
+                    <a class="text-orkut-pink underline mt-10" href="#">Esqueci minha senha</a>
                 </div>
             </form>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     "./src/**/*.{js,vue}"
   ],
   theme: {
+    colors: {
+      "orkut-pink": "#ED2590",
+      "white": "#FFFFFF"
+    },
     extend: {
       fontFamily: {
         'sf-pro': ['SF Pro Display', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
